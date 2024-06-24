565ff9ae43bd1096073bfbac1fcd9beac05b5dc5
corentinBeaujois <beaujoiscorentin@gmail.com>
2024-06-24 14:47:53 +0200

    add: 1 Game

--- src/page/game.tsx
+++ src/page/game.tsx
@@ -3,8 +3,21 @@ import React from "react";
 function Game(){
     return(
         <>
-            <div className="cardgametitle">
-                <img src="./card/Game Name.png" alt="title du jeu" />
+            <div className="cardsgamegroup">
+                <div className="cardgameleft">
+                    <img src="./card/Group-68.png" alt="carte du jeu gauche" />
+                </div>
+                <div className="cardsgamecenter">
+                    <div className="cardgametitle">
+                        <img src="./card/Game Name.png" alt="title du jeu" />
+                    </div>
+                    <div className="cardgamemain">
+                        <img src="./card/Group-14.png" alt="carte du jeu principal" />
+                    </div>
+                </div>
+                <div className="cardgameright">
+                    <img src="./card/Group-66.png" alt="carte du jeu droit" />
+                </div>
             </div>
         </>
     )
